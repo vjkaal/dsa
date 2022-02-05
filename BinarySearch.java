22e9aa8abe90cc87e4da272bdf29edddbe235212
vjkaal <vanshajkaushal@yahoo.com>
2022-02-06 02:01:06 +0530

    added support if elem at direct start or end of the array

--- BinarySearch.java
+++ BinarySearch.java
@@ -8,6 +8,19 @@ class BinarySearch{
       System.out.println("Array not sorted");
       return;
     }
+    // chk front
+    if(array[0] == elem){
+      System.out.println(elem + " found at start");
+      return;
+    }
+
+    //chk Rear
+    if(array[array.length-1] == elem){
+      System.out.println(elem + " found at rear");
+      return;
+    }
+
+    // proper binary search
     int res = bs(array, 0, array.length-1, elem);
     if(res >= 0) System.out.println(elem + " found at position " + res);
     else System.out.println(elem + " not found");
