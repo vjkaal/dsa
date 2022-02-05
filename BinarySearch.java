de10c986836d39f7e2f82c3672fd07f6f765eb4a
vjkaal <vanshajkaushal@yahoo.com>
2022-02-06 01:53:25 +0530

    added support if array not sorted

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,5 +1,9 @@
 class BinarySearch{
   public void search(int[] array, int elem){
+    if(!chkSorted(array)){
+      System.out.println("Array not sorted");
+      return;
+    }
     int res = bs(array, 0, array.length-1, elem);
     if(res >= 0) System.out.println(elem + " found at position " + res);
     else System.out.println(elem + " not found");
@@ -14,5 +18,10 @@ class BinarySearch{
       else return bs(array, mid+1, high, elem);
     }
     else return -1;
+  }
+
+  private boolean chkSorted(int[] array){
+    for(int i=0;i<array.length-1;i++) if(array[i] > array[i+1]) return false;
+    return true;
   }
 }
